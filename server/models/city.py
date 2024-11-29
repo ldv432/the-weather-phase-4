0e203eae8c2276ebd43c0ce7be2a81a8199de482
ldv432 <ldv432@gmail.com>
2024-11-28 19:46:52 -0700

    initial commit

---
+++ server/models/city.py
@@ -0,0 +1,25 @@
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.associationproxy import association_proxy
+
+from config import db
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class City(db.Model):
+    __tablename__ = 'cities'
+
+    #setting attributes
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False, unique=True)
+    lat = db.Column(db.Float, nullable=False)
+    long = db.Column(db.Float, nullable=False)
+
+    #setting relationships
+    favorites = db.relationship('Favorite', back_populates='city', cascade='all, delete-orphan')
+    current_wxs = db.relationship('CurrentWx', back_populates='city', cascade='all, delete-orphan')
+    forecasts = db.relationship('Forecast', back_populates='city', cascade='all, delete-orphan')
+
+    def __repr__(self):
+        return f"<City {self.name}>"
---
+++ server/models/favorite.py
@@ -0,0 +1,25 @@
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.associationproxy import association_proxy
+
+from config import db
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import validates
+
+db = SQLAlchemy()
+
+class Favorite(db.Model):
+    __tablename__ = 'favorites'
+
+    #setting attributes
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False, index=True)
+    city_id = db.Column(db.Integer, db.ForeignKey('cities.id'), nullable=False, index=True)
+    nickname = db.Column(db.String(40), nullable=True)
+
+    #setting relationships
+    user = db.relationship('User', back_populates='favorites')
+    city = db.relationship('City', back_populates='favorites')
+
+    def __repr__(self):
+        return f"<Favorite id={self.id} nickname={self.nickname}>"
